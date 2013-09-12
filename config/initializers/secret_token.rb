b04f1bc1a90aaf6ee8fa19e8c7575de17f320841
culaucon <pls@pls-VirtualBox.(none)>
2013-09-13 00:42:40 +0800

    nil

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Movie::Application.config.secret_key_base = '7423638d7d8b9d46f6cf6a5796d6187a30b0be17e3b07b46e163be72a3e9d2b9fcef5b6dbf5198ac7312d20d94cc70ee98974f59444576434d46243d71ce7fed'
